c40283303ab5715768b059dc19872156a5e6b278
王亦雪 <wangyixue@autoreg-082493.dyn.wpi.edu>
2017-01-25 16:58:42 -0500

    sure

--- PA2/PA2.c
+++ PA2/PA2.c
@@ -1,1 +1,40 @@
-new file
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]){
+    FILE *input;
+    int k, x, y, gens;
+    int *B[  ];
+    
+    if(argc < 5){
+        printf("Need 5 argcs!!!!\n");
+        exit(-1);}
+    
+    
+    
+    x = atoi(argv[1]);
+    y = atoi(argv[2]);
+    gens = atoi(argv[3]);
+    
+    
+    input = fopen(argv[4], "r");
+    
+    if(!input){
+        printf("Unable to open file\n");
+        exit(-1);}
+    
+    //int *B[ ];
+    unsigned int i, j, k;
+    B = malloc(x * sizeof(int *));
+    if (B){
+        for (i = 0; i < x; i++) {
+        B[i] = malloc(y * sizeof (int));
+        if (!B[i]) {exit(-1);}
+        }
+    }
+    
+    printf("succeed!\n");
+    return 0;
+}
